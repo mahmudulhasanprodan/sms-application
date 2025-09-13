3fa74c28a66263ee93ebb0f88d4e7c386d2064b5
mahmudulhasanprodan <mahmudulhasan8627@gmail.com>
2025-09-12 20:20:05 -0700

    User Page Creation Done

--- client/my-project/src/UserComponent/Userdetails.jsx
+++ client/my-project/src/UserComponent/Userdetails.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
+import { FaPlusCircle } from "react-icons/fa";
+import Logo from "../assets/Logo.png"
+import { AiFillDelete } from "react-icons/ai";
 
 const Userdetails = () => {
   return (
-   <>
-    <div className="bg-BgColor h-[100vh]">
-        <h1>Hello</h1>
-    </div>
-   </>
-  )
+    <>
+      <div className="bg-BgColor h-[100vh]">
+        <div className="flex items-center justify-center pt-6">
+          <div className="w-[1000px] h-[500px] bg-slate-700 rounded-lg">
+            <div className="flex flex-col items-center justify-center gap-y-3 pt-6">
+              <h2 className="font-Lato font-bold text-2xl text-white">
+                Manage Users
+              </h2>
+              <span className="font-Lato font-bold text-3xl text-white cursor-pointer">
+                <FaPlusCircle />
+              </span>
+            </div>
+            <div className="w-[900px] m-auto pt-6">
+              <ul className="flex items-center justify-between bg-BgColor py-2">
+                <li className="basis-1/5 pl-3 font-Lato font-bold text-xl">
+                  Name
+                </li>
+                <li className="basis-2/5 pl-8 font-Lato font-bold text-xl">
+                  Email
+                </li>
+                <li className="basis-1/5 pl-6 font-Lato font-bold text-xl">
+                  Manage
+                </li>
+              </ul>
+              {/* Users Details */}
+              <div className="flex items-center justify-between pt-2 border-b-[1px] pb-2">
+                <div className="flex items-center gap-x-3 basis-1/5 pl-3">
+                  <picture>
+                    <img src={Logo} alt={Logo} className="w-12 h-12"/>
+                  </picture>
+                  <h2 className="text-white font-Lato  text-sm">User2</h2>
+                </div>
+                <div className="basis-2/5 pl-8">
+                  <h2 className="font-Lato text-white text-sm">mahmudulhasan8627@gmail.com</h2>
+                </div>
+                <div  className="basis-1/5 pl-6">
+                  <span className="basis-1/5 font-Lato text-white active:text-red-400 text-xl cursor-pointer"><AiFillDelete /></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Userdetails
--- client/my-project/src/commonComponent/Header/Header.jsx
+++ client/my-project/src/commonComponent/Header/Header.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+
   return (
     <>
       <div className="bg-BgColor">
         <div>
           <ul className="flex items-center justify-center gap-x-3 pt-6">
-            <NavLink to={"/"}>
-              <li className="border-r-[1px] pr-2 border-white cursor-pointer font-Lato font-bold">
+            <NavLink to={"/inbox"}>
+              <li className="border-r-[1px] pr-2 border-white  cursor-pointer font-Lato font-bold">
                 Inbox
               </li>
             </NavLink>
-            <NavLink to={"/users"}>
-              <li className="border-r-[1px] pr-2 border-white cursor-pointer font-Lato font-bold">
+            <NavLink to={"/user"}>
+              <li className="border-r-[1px] pr-2 border-white  cursor-pointer font-Lato font-bold">
                 Users
               </li>
             </NavLink>
-            <NavLink to={"/signup"}>
-              <li className="cursor-pointer font-Lato font-bold">Login</li>
+            <NavLink to={"/"}>
+              <li className="cursor-pointer font-Lato font-bold focus:text-white">Login</li>
             </NavLink>
           </ul>
         </div>
